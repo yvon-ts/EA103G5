d4a92e037bb430f5de287ae0960dd0301abc7da4
Joeyulol <n8742977@gmail.com>
2020-11-03 15:55:20 +0800

    權限update joe

--- src/com/employee/login/controller/Emp_authFilter.java
+++ src/com/employee/login/controller/Emp_authFilter.java
@@ -51,7 +51,6 @@ public class Emp_authFilter implements Filter {
 		FUN0001.add(req.getContextPath() + "/back-end/employee/empall/newadd_emp.jsp");
 		FUN0001.add(req.getContextPath() + "/back-end/employee/empall/newupdate_emp.jsp");
 
-
 		List<String> FUN0002 = new ArrayList<>();
 		FUN0002.add(req.getContextPath() + "/back-end/members/select_members.jsp");
 		FUN0002.add(req.getContextPath() + "/back-end/members/listAllMembers.jsp");
@@ -62,8 +61,13 @@ public class Emp_authFilter implements Filter {
 		FUN0003.add(req.getContextPath() + "/back-end/teacher/showOneTeacher.jsp");
 		FUN0003.add(req.getContextPath() + "/back-end/teacher/listAllTeachersCompositeQuery.jsp");
 		FUN0003.add(req.getContextPath() + "/back-end/teacher/listAllTeachers.jsp");
-		
+
 		List<String> FUN0004 = new ArrayList<>();
+		FUN0004.add(req.getContextPath() + "/back-end/classroom/listAllClassroom.jsp");
+		FUN0004.add(req.getContextPath() + "/back-end/classroom/addClassroom.jsp");
+		FUN0004.add(req.getContextPath() + "/back-end/classroom/listOneClassroom.jsp");
+		FUN0004.add(req.getContextPath() + "/back-end/classroom/select_page.jsp");
+		FUN0004.add(req.getContextPath() + "/back-end/classroom/update_classroom_input.jsp");
 		FUN0004.add(req.getContextPath() + "/back-end/lecture/addLec.jsp");
 		FUN0004.add(req.getContextPath() + "/back-end/lecture/addLecIndex.jsp");
 		FUN0004.add(req.getContextPath() + "/back-end/lecture/lectureIndex.jsp");
@@ -71,15 +75,26 @@ public class Emp_authFilter implements Filter {
 		FUN0004.add(req.getContextPath() + "/back-end/lecture/listOneLec.jsp");
 		FUN0004.add(req.getContextPath() + "/back-end/lecture/select_page.jsp");
 		FUN0004.add(req.getContextPath() + "/back-end/lecture/updateLec.jsp");
+		FUN0004.add(req.getContextPath() + "/back-end/lecseat/listOneSeat.jsp");
 		FUN0004.add(req.getContextPath() + "/back-end/lecorder/listAllLodr.jsp");
-		FUN0004.add(req.getContextPath() + "/back-end/classroom/listAllClassroom.jsp");
-		FUN0004.add(req.getContextPath() + "/back-end/classroom/addClassroom.jsp");
-		FUN0004.add(req.getContextPath() + "/back-end/classroom/listOneClassroom.jsp");
-		FUN0004.add(req.getContextPath() + "/back-end/classroom/select_page.jsp");
-		FUN0004.add(req.getContextPath() + "/back-end/classroom/update_classroom_input.jsp");
+		FUN0004.add(req.getContextPath() + "/back-end/speaker/addSpkr.jsp");
+		FUN0004.add(req.getContextPath() + "/back-end/speaker/listAllSpkr.jsp");
+		FUN0004.add(req.getContextPath() + "/back-end/speaker/listOneSpkr.jsp");
+		FUN0004.add(req.getContextPath() + "/back-end/speaker/select_page.jsp");
+		FUN0004.add(req.getContextPath() + "/back-end/speaker/updateSpkr.jsp");
 		
 		
-		
+		List<String> FUN0005 = new ArrayList<>();
+		FUN0005.add(req.getContextPath() + "/back-end/Order_Master/ListAllOrder.jsp");
+		FUN0005.add(req.getContextPath() + "/back-end/Order_Master/ListOneOrder.jsp");
+		FUN0005.add(req.getContextPath() + "/back-end/Order_Master/OrderMasterDB.jsp");
+		FUN0005.add(req.getContextPath() + "/back-end/Order_Master/update_order_input.jsp");
+		FUN0005.add(req.getContextPath() + "/back-end/Order_Detail/listAllOrderDetail.jsp");
+		FUN0005.add(req.getContextPath() + "/back-end/Order_Detail/listOneOrderDetail.jsp");
+		FUN0005.add(req.getContextPath() + "/back-end/Order_Detail/listOneOrderNo.jsp");
+		FUN0005.add(req.getContextPath() + "/back-end/Order_Detail/update_order_input.jsp");
+		FUN0005.add(req.getContextPath() + "/back-end/course/editCourseStatus.jsp");
+
 		if (list.contains("FUN0001") && FUN0001.contains(urls)) {
 			chain.doFilter(request, response);
 		} else if (list.contains("FUN0002") && FUN0002.contains(urls)) {
@@ -88,7 +103,9 @@ public class Emp_authFilter implements Filter {
 			chain.doFilter(request, response);
 		} else if (list.contains("FUN0004") && FUN0004.contains(urls)) {
 			chain.doFilter(request, response);
-		}else {
+		} else if (list.contains("FUN0005") && FUN0005.contains(urls)) {
+			chain.doFilter(request, response);
+		} else {
 			session.setAttribute("error", "你沒有權限!");
 			System.out.println("跑到這");
 			res.sendRedirect(req.getContextPath() + "/back-end/employee/loginsuccess.jsp");
