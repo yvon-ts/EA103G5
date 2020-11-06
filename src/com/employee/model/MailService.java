79a7c7fd573c4da24f9f4d101dea67d8c0f08c8a
Joeyulol <n8742977@gmail.com>
2020-11-06 10:07:19 +0800

    更換mail joe

--- src/com/employee/model/MailService.java
+++ src/com/employee/model/MailService.java
@@ -26,8 +26,8 @@ public class MailService {
 
        // ●設定 gmail 的帳號 & 密碼 (將藉由你的Gmail來傳送Email)
        // ●須將myGmail的【安全性較低的應用程式存取權】打開
-	     final String myGmail = "furongkuang9@gmail.com";
-	     final String myGmail_password = "1qaz2wsx3edc--";
+	     final String myGmail = "n8742977@gmail.com";
+	     final String myGmail_password = "n0975010771";
 		   Session session = Session.getInstance(props, new Authenticator() {
 			   protected PasswordAuthentication getPasswordAuthentication() {
 				   return new PasswordAuthentication(myGmail, myGmail_password);
